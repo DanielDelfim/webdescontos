fc10fde0824d9690ffc63248c4c5ff31da6e4673
Daniel Delfim <dmdelfim@gmail.com>
2019-04-15 11:46:08 -0300

    first commit

---
+++ src/main/java/com/danieldelfim/webdescontos/WebdescontosApplication.java
@@ -0,0 +1,13 @@
+package com.danieldelfim.webdescontos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdescontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdescontosApplication.class, args);
+	}
+
+}
